cbec8138d08f438d751ae0982b8c4948a4ecda8b
k.kozlowski <k.kozlowski@reply.com>
2025-03-05 11:15:25 +0100

    Initial commit

---
+++ src/main/java/pl/coderion/myspringapp/MyspringappApplication.java
@@ -0,0 +1,13 @@
+package pl.coderion.myspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderion/myspringapp/MyspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderion.myspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
